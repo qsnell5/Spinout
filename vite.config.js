46d1e8a1eab7117cdbf4adb07f7c029cc34131c5
qsnell5 <qmsnell@outlook.com>
2026-04-07 15:46:57 -0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,14 @@
-node_modules
-dist
-.netlify
-.env
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    proxy: {
+      '/.netlify/functions': {
+        target: 'http://localhost:8888',
+        changeOrigin: true,
+      },
+    },
+  },
+})
